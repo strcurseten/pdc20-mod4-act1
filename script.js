317541c1f23bd1611db081ea274953de3d14a179
= <=>
2022-10-07 16:23:38 +0800

    Commit all files

---
+++ script.js
@@ -0,0 +1,12 @@
+function msg() {
+    alert("_World by Seventeen");
+}
+
+function svt() {
+    let name="Kirsten";
+            let a = 10;
+            let b = 7;
+            let c = "Say the name";
+
+            alert(`Hello, ${name}. If ${a} is added to ${b}\n${c}; ${a+b}!`);
+}
